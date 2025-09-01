2d7e0288537093b4768d600699a7376ad9b9c9fe
ManojkumarKamaraj <manomk2020@gmail.com>
2025-09-01 12:35:16 +0530

    new python test file

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python
+
+WORKDIR /
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 5000
+
+COPY app.py .
+
+CMD ["python","baseprogram.py"]
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import FLASK,Response
+from tabulate import tabulate
+
+app = FLASK(__name__)
+
+data=[
+["row1-col-1","row1-col-2","row1-col-3"],
+["row2-col-1","row2-col-2","row3-col-3"],
+["row3-col-1","row3-col-2","row3-col-3"]
+]
+
+headers=[
+["column1","column2","column3"]
+]
+
+app.route('/5000')
+
+def index():
+     table=tabulate(data,headers,tablefmt="grid");
+     return Response(f"<pre>{table}</pre>",mimetype="text/html");
+
+if __name__=="__main__":
+    app.run(debug=True,port=5000)
